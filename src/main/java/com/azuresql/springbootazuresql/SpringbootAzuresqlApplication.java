f01e9ed6000b78e566bbd4a7f14587a51fc80d51
utkarshsanju13 <utkarshsanju13@gmail.com>
2023-02-18 17:47:50 +0530

    first commit

---
+++ src/main/java/com/azuresql/springbootazuresql/SpringbootAzuresqlApplication.java
@@ -0,0 +1,13 @@
+package com.azuresql.springbootazuresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAzuresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAzuresqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azuresql/springbootazuresql/SpringbootAzuresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azuresql.springbootazuresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAzuresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
