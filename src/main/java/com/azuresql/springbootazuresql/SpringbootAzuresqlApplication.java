34b1b1ec45b8998a59569e768039b8788c6b2280
utkarshsanju13 <utkarshsanju13@gmail.com>
2023-02-20 12:48:25 +0530

    changes

--- src/main/java/com/azuresql/springbootazuresql/SpringbootAzuresqlApplication.java
+++ src/main/java/com/azuresql/springbootazuresql/SpringbootAzuresqlApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-//@EntityScan
 //@EnableAutoConfiguration
 public class SpringbootAzuresqlApplication {
 
